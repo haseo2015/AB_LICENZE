484cc9827e898f262e08a134a53c4ebe3c84a06a
haseo2015 <fabio.monti@gmail.com>
2016-04-16 14:42:02 +0200

    init classi

---
+++ class.config.php
@@ -0,0 +1,11 @@
+<?php
+    namespace config;
+    class config {
+        protected $dbuser = "AB_user";
+        protected $dbpass = "AB_pass";
+        protected $dbname = "ab_licenze";
+        protected $EXTdbuser = "";
+        protected $EXTdbpass = "";
+        protected $EXTdbname = "";
+
+    }
